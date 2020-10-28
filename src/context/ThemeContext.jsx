8e051a135cd875a96f89131ffd3faceee772e31f
Heather Peterson <hpeterson462@gmail.com>
2020-10-28 16:31:06 -0700

    theme context updated

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 
 export const ThemeContext = React.createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [lightColor, setLightColor] = useState('#FFFFF');
-  const [darkColor, setDarkColor] = useState('#00000');
-  const [text, setText] = useState('');
+  const [themeType, setThemeType] = useState('Light');
 
-  const handleChange = ({ target }) => {
-    if (target.name === 'lightColor') setLightColor(target.value);
-    if (target.name === 'darkColor') setDarkColor(target.value);
+  const toggle = () => {
+    setThemeType(oldType => {
+      if (oldType === 'Light') return 'Dark';
+      return 'Light';
+    });
   };
 
-  const bucket = {
-    lightColor,
-    darkColor,
-    handleChange
-  };
+  // const handleChange = ({ target }) => {
+  //   if (target.name === 'lightColor') setLightColor(target.value);
+  //   if (target.name === 'darkColor') setDarkColor(target.value);
+  // };
+
+  // const bucket = {
+  //   lightColor,
+  //   darkColor,
+  //   handleChange
+  // };
 
   return (
-    <ThemeContext.Provider value={bucket}>
+    <ThemeContext.Provider value={{ themeType, toggle }}>
       {children}
     </ThemeContext.Provider>
   );
