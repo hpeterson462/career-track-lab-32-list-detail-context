b40b604420122ccd59eb0743a2f68aba4b651780
Heather Peterson <hpeterson462@gmail.com>
2020-10-28 12:07:02 -0700

    installed react-router-dom and built router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import CharacterList from '../app/characters/CharacterList';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={CharacterList} />
+      </Switch>
+    </Router>
+  );
 }
