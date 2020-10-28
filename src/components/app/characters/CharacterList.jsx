2609f768a4c81279fbd3f3851468b6fc07d61fd6
Heather Peterson <hpeterson462@gmail.com>
2020-10-28 15:34:12 -0700

    css file

--- src/components/app/characters/CharacterList.jsx
+++ src/components/app/characters/CharacterList.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import CharacterItem from './CharacterItem';
 import { useCharacters } from '../../../hooks/character';
+import styles from './CharacterList.css';
 
 const CharacterList = () => {
   const characters = useCharacters();
 
   const characterElements = characters.map(character => (
-    < li key={character.char_id} >
+    <li key={character.char_id}>
       <CharacterItem {...character} />
     </li >
   ));
 
   return (
-    <ul data-testid="characters">
+    <ul data-testid="characters" className={styles.CharacterList}>
       {characterElements}
     </ul>
   );
